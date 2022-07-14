86df5e667a868e72caef2282996732c56fb69cf7
Vadim Bayramov <bayramovva@gmail.com>
2022-07-15 01:34:58 +0300

    Add Zadacha13

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,9 @@
+﻿//Random rand = new Random();
+//int number = rand.Next();
+int number = 12;
+Console.WriteLine(number);
+
+int third = number % 1000 / 100;
+Console.WriteLine(third);
+
+// пока нет решения
