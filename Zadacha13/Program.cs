cf955ab915bb25aace98c19f86c362be0b7168a4
Vadim <bayramovva@gmail.com>
2022-07-15 16:04:56 +0300

    Add Zadacha 13

--- Zadacha13/Program.cs
+++ Zadacha13/Program.cs
@@ -1,9 +1,21 @@
-﻿//Random rand = new Random();
-//int number = rand.Next();
-int number = 12;
-Console.WriteLine(number);
+﻿Random rand = new Random();
+int number = rand.Next();
+Console.WriteLine(number); //Вывод всего числа
 
-int third = number % 1000 / 100;
-Console.WriteLine(third);
+int newNumber = 1000;
+while (newNumber < number)
+{
+    number = number / 10;
+}
+//Console.WriteLine(number); //Вывод первых трех разрядов
 
-// пока нет решения
+int newNumber_2 = 100;
+if (newNumber_2 <= number)
+{
+    number = number % 10;
+    Console.WriteLine(number + " -третья цифра изначально числа");
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
